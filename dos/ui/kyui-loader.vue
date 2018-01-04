5a6f9d9f1c7b4e438a2aafedbd420c377f9304a8
Qiu <smallerqiu@gmail.com>
2018-01-04 16:39:01 +0800

    build

--- dos/ui/kyui-loader.vue
+++ dos/ui/kyui-loader.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <h2>KYUI-Loader</h2>
+        <h2>KUI-Loader</h2>
         <h3>用途</h3>
         <p>在vue 里面，自定义组件名称是无法和 原生的html组件 重名的， 在vue1.x 版本有这个限制，但是在vue2.x 以后，有虚拟DOM ，所以可以使用原生组件来命名自定义组件，但是有大小写区分(emplate/render模式)</p>
         <h3>使用方法</h3>
         <h4>安装</h4>
         <p>通过 npm 安装 kui-loader</p>
-        <Code>npm i kyui-loader --save-dev</Code>
+        <Code>npm i kui-loader --save-dev</Code>
         <h4>配置</h4>
         <p>配置 webpack，修改 vue-loader 的配置，如下：</p>
         <Code>{{webconfig}}</Code>
