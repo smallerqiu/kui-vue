f152eb5d8494531715fde4827eaf13eea873cb45
Qiu <smallerqiu@gmail.com>
2022-01-20 14:57:29 +0800

    加入语言包

---
+++ dist/locale/ja.js
@@ -0,0 +1,8 @@
+/*!
+ * kui-vue v3.2.6 
+ * Copyright 2017-present, kui-vue.
+ * All rights reserved.
+ * Author: chuchur@qq.com / www.chuchur.com
+ *         
+ */
+kui.lang.ja=function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}return r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=5)}({5:function(e,t,r){"use strict";r.r(t),t.default={k:{datePicker:{startDate:"開始日",endDate:"終了日",placeholder:"日付を選択",months:["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],weeks:["日","月","火","水","木","金","土"],year:"",month:"",day:"",today:"今日",now:"現在",ok:"OK",selectDate:"日付を選択",selectTime:"時間を選択"},drawer:{ok:"OK",cancel:"キャンセル"},empty:{description:"データなし"},pop:{ok:"OK",cancel:"キャンセル"},modal:{ok:"OK",cancel:"キャンセル"},page:{page:"",pageSize:"件/ページ",goto:"ジャンプ先",items:"",total:"総計"},select:{placeholder:"選択してください",loading:"ロード中...",emptyText:"データなし"},table:{emptyText:"データなし"},upload:{successful:"成功",failed:"失敗",errorFileSize:"不法"}}}}}).default;
