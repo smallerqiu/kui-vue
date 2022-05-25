0cf8d0c51ff23496dd58ac8d33e26d049fcb2a1d
chuchur <chuchur@qq.com>
2022-05-25 18:22:18 +0800

    Grid 支持垂直间距和修复bug Slider 修复Step，Min 值等问题

--- components/grid/col.jsx
+++ components/grid/col.jsx
@@ -23,17 +23,46 @@ export default {
   render() {
     let { offset, span, $slots, flex } = this
     const gutter = this.Row.gutter
-    const props = {
+    let props = {
       class: [`k-col`, {
         [`k-col-${span}`]: span,
-        [`k-col-offset-${offset}`]: offset > 0 && offset <= 24
+        // [`k-col-offset-${offset}`]: offset > 0 && offset <= 24
       }],
       style: {
-        paddingLeft: gutter ? `${gutter / 2}px` : null,
-        paddingRight: gutter ? `${gutter / 2}px` : null,
-        flex: flex ? this.parseFlex(flex) : null
+        // paddingLeft: gutter ? `${gutter / 2}px` : null,
+        // paddingRight: gutter ? `${gutter / 2}px` : null,
+        // flex: flex ? this.parseFlex(flex) : null
       }
     }
+    // console.log(gutter)
+    if (Array.isArray(gutter)) {
+      let [v = 0, h = 0] = gutter;
+      if ((v == h && v > 0)) {
+        props.style.padding = `${v / 2}px`
+      } else if (v > 0 && h > 0) {
+        props.style.padding = `${h / 2}px ${v / 2}px`
+      } else {
+        if (v > 0) {
+          props.style.paddingLeft = `${v / 2}px`
+          props.style.paddingRight = `${v / 2}px`
+        }
+        if (h > 0) {
+          props.style.paddingTop = `${v / 2}px`
+          props.style.paddingTop = `${v / 2}px`
+        }
+      }
+
+    } else if (!isNaN(Number(gutter)) && gutter > 0) {
+      props.style.paddingLeft = `${gutter / 2}px`
+      props.style.paddingRight = `${gutter / 2}px`
+    }
+    if (flex) {
+      props.style.flex = this.parseFlex(flex)
+    }
+    if (offset > 0 && offset <= 24) {
+      props.class.push(`k-col-offset-${offset}`)
+    }
+
     return (
       <div {...props}>
         {$slots.default}
