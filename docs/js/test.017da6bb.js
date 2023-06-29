6f2b79b3d64c35451a40e2269d88fead04c365c1
Qiu <smallerqiu@gmail.com>
2023-06-29 20:59:04 +0800

    修复table组建

--- docs/js/test.017da6bb.js
+++
@@ -1,7 +0,0 @@
-/*!
- * kui-vue v3.3.5-a
- * Copyright 2017-present, kui-vue.
- * All rights reserved.
- * Author: chuchur@qq.com / www.chuchur.com
- */
-"use strict";(self["webpackChunkkui_vue"]=self["webpackChunkkui_vue"]||[]).push([[5043],{3476:function(e,a,t){t.r(a),t.d(a,{default:function(){return d}});var n=function(){var e=this,a=e._self._c;return a("Table",{attrs:{data:e.data,columns:e.columns,width:1800}},[a("a",{attrs:{slot:"action"},slot:"action"},[e._v("action")])])},u=[],s=(t(7658),{data(){const e=[];for(let a=0;a<10;a++)e.push({title:"Col"+a,key:"address"});return{data:[{key:0,name:"Li Lei",age:28,address:"Hubei Wuhan Guanggu SoftBase No.128",ellipsis:!0},{key:1,name:"Liu Hao",age:28,address:"Hubei"},{key:2,name:"Wang Kang",age:28,address:"Hubei Wuhan Guanggu Nanhu No.128"},{key:3,name:"Hu Cong",age:28,address:"Hubei Wuhan Guanggu Hongshan No.128"}],columns:[{title:"Name",key:"name",fixed:"left"},{title:"Age",key:"age",fixed:"left"},...e,{title:"Action",key:"action",fixed:"right"}]}}}),i=s,l=t(1001),o=(0,l.Z)(i,n,u,!1,null,null,null),d=o.exports}}]);
