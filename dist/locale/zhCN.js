8a47f250fd9e17d074d046289cf7c6a8d144eb14
chuchur <chuchur@qq.com>
2024-01-30 00:35:59 +0800

    up ver

--- dist/locale/zhCN.js
+++ dist/locale/zhCN.js
@@ -1,9 +1,9 @@
 /*!
- * kui-vue v3.3.6
+ * kui-vue v3.3.7
  *  Copyright 2017-present, kui-vue.
  *  All rights reserved.
  *  Docs: https://k-ui.cn
  * Author: chuchur@qq.com / www.chuchur.com
  *
  */
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define("kui_lang_zhCN",[],t):"object"==typeof exports?exports.kui_lang_zhCN=t():e.kui_lang_zhCN=t()}(this,(function(){return e={285:function(e){e.exports={k:{datePicker:{startDate:"开始日期",endDate:"结束日期",placeholder:"请选择日期",months:["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],weeks:["一","二","三","四","五","六","日"],year:"年",month:"月",day:"日",today:"今天",now:"此刻",ok:"确定",selectDate:"选择日期",selectTime:"选择时间",back:"返回"},drawer:{ok:"确定",cancel:"取消"},empty:{description:"暂无数据"},pop:{ok:"确定",cancel:"取消"},modal:{ok:"确定",cancel:"取消"},page:{page:"页",pageSize:"条/页",goto:"跳至",items:"条数据",total:"共"},select:{placeholder:"请选择",loading:"加载中...",emptyText:"暂无数据"},table:{emptyText:"暂无数据"},upload:{successful:"上传成功",failed:"上传失败",errorFileSize:"文件尺寸不合法"}}}}},t={},function o(a){var r=t[a];if(void 0!==r)return r.exports;var n=t[a]={exports:{}};return e[a](n,n.exports,o),n.exports}(285);var e,t}));
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define("kui_lang_zhCN",[],t):"object"==typeof exports?exports.kui_lang_zhCN=t():e.kui_lang_zhCN=t()}(this,(function(){return e={285:function(e){e.exports={k:{datePicker:{startDate:"开始日期",endDate:"结束日期",placeholder:"请选择日期",months:["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],weeks:["日","一","二","三","四","五","六"],year:"年",month:"月",day:"日",today:"今天",now:"此刻",ok:"确定",selectDate:"选择日期",selectTime:"选择时间",back:"返回"},drawer:{ok:"确定",cancel:"取消"},empty:{description:"暂无数据"},pop:{ok:"确定",cancel:"取消"},modal:{ok:"确定",cancel:"取消"},page:{page:"页",pageSize:"条/页",goto:"跳至",items:"条数据",total:"共"},select:{placeholder:"请选择",loading:"加载中...",emptyText:"暂无数据"},table:{emptyText:"暂无数据"},upload:{successful:"上传成功",failed:"上传失败",errorFileSize:"文件尺寸不合法"}}}}},t={},function o(a){var r=t[a];if(void 0!==r)return r.exports;var n=t[a]={exports:{}};return e[a](n,n.exports,o),n.exports}(285);var e,t}));
