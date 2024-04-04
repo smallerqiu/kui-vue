7f404ae07d4f6d1e9b9d5510a5d8e0fb1b6fe693
chuchur <chuchur@qq.com>
2024-04-04 15:47:22 +0800

    优化细节

---
+++ docs/js/test.89c14.js
@@ -0,0 +1,9 @@
+/*!
+ * kui-vue v3.3.8
+ *  Copyright 2017-present, kui-vue.
+ *  All rights reserved.
+ *  Docs: https://k-ui.cn
+ * Author: chuchur@qq.com / www.chuchur.com
+ *
+ */
+"use strict";(self.webpackChunkkui_vue=self.webpackChunkkui_vue||[]).push([[5043],{9307:function(e,t,a){a.r(t),a.d(t,{default:function(){return u}});var r=function(){var e=this,t=e._self._c;return t("div",[t("DatePicker",{attrs:{value:"2019-10-12 22:12:12",format:"YYYY年MM月DD日 HH:mm:ss",mode:"dateTime"}}),e._v(" "),t("br"),e._v(" "),t("DatePicker",{attrs:{value:"2019-10-12",format:"YYYY/MM/DD"}}),e._v(" "),t("br"),e._v(" "),t("DatePicker",{attrs:{value:"2019-10-12",format:"YYYY.MM.DD"}}),e._v(" "),t("br"),e._v(" "),t("DatePicker",{attrs:{format:"YYYY.MM.DD",mode:"dateRange"},on:{change:e.change},model:{value:e.date,callback:function(t){e.date=t},expression:"date"}}),e._v(e._s(e.date)+"\n")],1)};r._withStripped=!0;var n={data(){return{date:["2019-10-12","2020-10-19"]}},methods:{change(e){console.log(e)}}},u=(0,a(1900).Z)(n,r,[],!1,null,null,null).exports}}]);
