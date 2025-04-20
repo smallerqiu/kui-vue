e33c7756cdb96ed3a6ee3ed15fd206cba8e34325
Qiu <smallerqiu@gmail.com>
2025-04-20 20:39:46 +0800

    md docs

---
+++ docs/js/home.e7432.js
@@ -0,0 +1,9 @@
+/*!
+ * kui-vue v3.4.9
+ *  Copyright 2017-present, kui-vue.
+ *  All rights reserved.
+ *  Docs: https://k-ui.cn
+ * Author: chuchur@qq.com / www.chuchur.com
+ *
+ */
+"use strict";(self.webpackChunkkui_vue=self.webpackChunkkui_vue||[]).push([[7962],{5989:function(t,e,r){r.r(e),r.d(e,{default:function(){return u}});var i=function(){var t=this,e=t._self._c;return e("Layout",{staticClass:"index"},[e("Header"),t._v(" "),e("section",{staticClass:"index-content"},[e("h1",[t._v("一套基于Vue.js的桌面UI组件库")]),t._v(" "),e("div",{staticClass:"btn-content"},[e("Button",{staticClass:"start",attrs:{icon:t.CaretForwardCircle,size:"large"},on:{click:t.start}},[t._v("开始使用")]),t._v(" "),e("Button",{attrs:{icon:t.LogoGitee,size:"large",type:"dashed",theme:"light"},on:{click:t.gitee}},[t._v("Gitee")])],1)]),t._v(" "),e("Footer")],1)};i._withStripped=!0;r(1479);var s=r(3190),a=r(7837),o=r(3686),n={data(){return{LogoGitee:o.LogoGitee,CaretForwardCircle:o.CaretForwardCircle}},components:{Header:s.A,Footer:a.A},methods:{start(){this.$router.push("/start/getting-started")},gitee(){window.open("//gitee.com/chuchur/kui-vue")}}},u=(0,r(1150).A)(n,i,[],!1,null,null,null).exports}}]);
