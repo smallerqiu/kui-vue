c50429446564bd4428964d7bd802e15697a5d9e1
Qiu <smallerqiu@gmail.com>
2025-05-11 23:38:30 +0800

    fix: tabs and etc.

--- components/affix/affix.jsx
+++ components/affix/affix.jsx
@@ -1,5 +1,4 @@
-
-import resize from '../_tool/resize'
+import resize from "../_tool/resize";
 
 // function getStyle(el, attr) {
 //   if (el.currentStyle) {
@@ -10,12 +9,17 @@ import resize from '../_tool/resize'
 // }
 
 export default {
-  name: 'Affix',
+  name: "Affix",
   directives: { resize },
   props: {
     offsetTop: { type: Number, default: 0 },
     offsetBottom: Number,
-    target: { type: Function, default: () => { return window } }
+    target: {
+      type: Function,
+      default: () => {
+        return window;
+      },
+    },
   },
   data() {
     return {
@@ -23,65 +27,67 @@ export default {
       width: 0,
       height: 0,
       offsetTopValue: 0,
-    }
+    };
   },
   beforeDestory() {
     if (this.target() == window) {
-      target.removeEventListener('scroll', this.updatePosition)
+      target.removeEventListener("scroll", this.updatePosition);
     }
   },
   mounted() {
-    let target = this.target()
-    if (target && !this.$isServer) {
-      target.addEventListener('scroll', this.updatePosition)
+    let target = this.target();
+    if (target && typeof window !== "undefined") {
+      target.addEventListener("scroll", this.updatePosition);
     }
   },
   methods: {
     updatePosition(e) {
-      let { offsetBottom, offsetTop, $refs } = this
-      let target = this.target()
+      let { offsetBottom, offsetTop, $refs } = this;
+      let target = this.target();
       if (!$refs.blob || !target) return;
 
       let rect = $refs.blob.getBoundingClientRect();
-      let container = (target != window && target != document) ? target.getBoundingClientRect() : { top: 0, bottom: document.documentElement.clientHeight }
+      let container = target != window && target != document ? target.getBoundingClientRect() : { top: 0, bottom: document.documentElement.clientHeight };
 
-      let hasbottom = typeof offsetBottom == 'number' && offsetBottom >= 0
-      let fx = hasbottom ? container.bottom - rect.bottom >= offsetBottom : rect.top - container.top <= offsetTop
+      let hasbottom = typeof offsetBottom == "number" && offsetBottom >= 0;
+      let fx = hasbottom ? container.bottom - rect.bottom >= offsetBottom : rect.top - container.top <= offsetTop;
 
       if (!fx) {
-        this.offsetTopValue = rect.top
+        this.offsetTopValue = rect.top;
         if (target == window || target == document) {
           if (this.offsetTopValue > offsetTop && !hasbottom) {
-            this.offsetTopValue = offsetTop
+            this.offsetTopValue = offsetTop;
           }
           if (hasbottom && container.bottom - rect.bottom < offsetBottom) {
-            this.offsetTopValue = container.bottom - offsetBottom - rect.height
+            this.offsetTopValue = container.bottom - offsetBottom - rect.height;
           }
         } else {
           //todo:
         }
       }
       if (this.fixed != fx) {
-        this.fixed = fx
-        this.width = rect.width
-        this.height = rect.height
-        this.$emit('change', this.fixed)
+        this.fixed = fx;
+        this.width = rect.width;
+        this.height = rect.height;
+        this.$emit("change", this.fixed);
       }
-    }
+    },
   },
   render() {
-    let blobStyle = null, fixedStyle = null, classes = null;
+    let blobStyle = null,
+      fixedStyle = null,
+      classes = null;
     if (this.fixed) {
       blobStyle = {
         width: `${this.width}px`,
-        height: `${this.height}px`
-      }
+        height: `${this.height}px`,
+      };
       fixedStyle = {
         width: `${this.width}px`,
         height: `${this.height}px`,
-        top: `${this.offsetTopValue}px`
-      }
-      classes = { ['k-affix']: this.fixed }
+        top: `${this.offsetTopValue}px`,
+      };
+      classes = { ["k-affix"]: this.fixed };
     }
 
     return (
@@ -90,6 +96,6 @@ export default {
           {this.$slots.default}
         </div>
       </div>
-    )
-  }
-}
+    );
+  },
+};
--- components/modal/modal.jsx
+++ components/modal/modal.jsx
@@ -1,10 +1,10 @@
-import Icon from '../icon'
-import { Button } from '../button'
+import Icon from "../icon";
+import { Button } from "../button";
 import transfer from "../_tool/transfer";
-import { measureScrollBar, getOffset } from '../_tool/utils'
-import { t } from '../locale'
+import { measureScrollBar, getOffset } from "../_tool/utils";
+import { t } from "../locale";
 // let cacheBodyOverflow = {};
-import { Close } from 'kui-icons'
+import { Close } from "kui-icons";
 
 export default {
   directives: { transfer },
@@ -31,42 +31,42 @@ export default {
       rendered: false,
       show: this.value,
       showInner: this.value,
-      left: '',
+      left: "",
       currentTop: this.top,
       isMouseDown: false,
       mousedownIn: false,
       startPos: { x: 0, y: 0 },
-      showPoint: { x: 0, y: 0 }
-    }
+      showPoint: { x: 0, y: 0 },
+    };
   },
   watch: {
     value(value) {
-      this.updateProp(value)
-    }
+      this.updateProp(value);
+    },
   },
   updated() {
     if (this.show) {
-      this.setPos()
+      this.setPos();
     }
   },
   methods: {
     updateProp(visible) {
       if (visible) {
-        this.rendered = true
-        this.$nextTick(e => {
-          this.show = visible
-          this.showInner = visible
-        })
+        this.rendered = true;
+        this.$nextTick((e) => {
+          this.show = visible;
+          this.showInner = visible;
+        });
       } else {
-        this.show = false
+        this.show = false;
         setTimeout(() => {
-          this.showInner = false
+          this.showInner = false;
         }, 300);
       }
       // this.resetBodyStyle(visible)
     },
     // resetBodyStyle(opened) {
-    //   if (!this.$isServer) {
+    //   if (!typeof window !== "undefined") {
     //     let target = document.body;
     //     if (!this.show && !cacheBodyOverflow.hasOwnProperty('overflow')) {
     //       cacheBodyOverflow = {
@@ -96,134 +96,158 @@ export default {
     // },
     setPos() {
       if (this.show && this.$refs.modal) {
-        let { showPoint: { x, y } } = this
+        let {
+          showPoint: { x, y },
+        } = this;
         // let { x, y } = showPoint
-        let { left, top } = getOffset(this.$refs.modal)
-        this.$refs.modal.style['transform-origin'] = `${x - left}px ${y - top}px`
+        let { left, top } = getOffset(this.$refs.modal);
+        this.$refs.modal.style["transform-origin"] = `${x - left}px ${y - top}px`;
       }
     },
     ok() {
-      this.$emit('ok')
+      this.$emit("ok");
     },
     cancel() {
-      this.close()
-      this.$emit('cancel')
+      this.close();
+      this.$emit("cancel");
     },
     close() {
-      this.$emit('input', false)
-      this.$emit('cancel')
-      this.$emit('close')
+      this.$emit("input", false);
+      this.$emit("cancel");
+      this.$emit("close");
     },
     clickMaskToClose(e) {
       // console.log(this.mousedownIn)
       if (!this.loading && this.maskClosable && !this.$refs.modal.contains(e.target) && !this.mousedownIn) {
-        this.close()
+        this.close();
       }
     },
     mousemove(e) {
       if (this.isMouseDown && this.draggable) {
-        let { x, y } = this.startPos
-        this.left += e.clientX - x
-        this.currentTop = this.currentTop || 100
-        this.currentTop += e.clientY - y
-        this.startPos = { x: e.clientX, y: e.clientY }
-        this.setPos()
-        e.preventDefault()
+        let { x, y } = this.startPos;
+        this.left += e.clientX - x;
+        this.currentTop = this.currentTop || 100;
+        this.currentTop += e.clientY - y;
+        this.startPos = { x: e.clientX, y: e.clientY };
+        this.setPos();
+        e.preventDefault();
       }
     },
     mouseup(e) {
-      this.isMouseDown = false
-      document.removeEventListener('mousemove', this.mousemove)
-      document.removeEventListener('mouseup', this.mouseup)
+      this.isMouseDown = false;
+      document.removeEventListener("mousemove", this.mousemove);
+      document.removeEventListener("mouseup", this.mouseup);
     },
     mousedown(e) {
       if (!this.show) {
-        this.showPoint = { x: e.clientX, y: e.clientY }
+        this.showPoint = { x: e.clientX, y: e.clientY };
       }
       if (e.button == 0 && this.draggable === true && this.$refs.hRef && this.$refs.hRef.contains(e.target)) {
-        this.isMouseDown = true
-        this.startPos = { x: e.clientX, y: e.clientY }
-        this.mousemove(e)
-        document.addEventListener('mousemove', this.mousemove)
-        document.addEventListener('mouseup', this.mouseup)
+        this.isMouseDown = true;
+        this.startPos = { x: e.clientX, y: e.clientY };
+        this.mousemove(e);
+        document.addEventListener("mousemove", this.mousemove);
+        document.addEventListener("mouseup", this.mouseup);
       }
 
-      this.mousedownIn = this.show && this.$refs.modal && this.$refs.modal.contains(e.target)
-    }
+      this.mousedownIn = this.show && this.$refs.modal && this.$refs.modal.contains(e.target);
+    },
   },
   beforeDestroy() {
-    !this.$isServer && document.removeEventListener('mousedown', this.mousedown);
+    typeof window !== "undefined" && document.removeEventListener("mousedown", this.mousedown);
     // this.resetBodyStyle(false)
   },
 
   mounted() {
-    if (!this.$isServer) {
-      document.addEventListener('mousedown', this.mousedown)
+    if (typeof window !== "undefined") {
+      document.addEventListener("mousedown", this.mousedown);
 
       if (this.draggable) {
-        this.left = (document.body.offsetWidth - (this.width || 520)) / 2
+        this.left = (document.body.offsetWidth - (this.width || 520)) / 2;
       }
       // console.log(this.value)
       if (this.value) {
         this.$nextTick(() => {
-          this.updateProp(true)
-        })
+          this.updateProp(true);
+        });
       }
     }
   },
   render() {
-    let { $slots, show, showInner, draggable, transfer } = this
+    let { $slots, show, showInner, draggable, transfer } = this;
 
     //mask
-    let maskNode = null
+    let maskNode = null;
     if (this.mask) {
-      maskNode = <transition name="k-modal-fade"><div class="k-modal-mask" v-show={show} /></transition>
+      maskNode = (
+        <transition name="k-modal-fade">
+          <div class="k-modal-mask" v-show={show} />
+        </transition>
+      );
     }
-    let okText = this.okText || t('k.modal.ok')
-    let cancelText = this.cancelText || t('k.modal.cancel')
+    let okText = this.okText || t("k.modal.ok");
+    let cancelText = this.cancelText || t("k.modal.cancel");
     //content
-    let contentNode = $slots.content
+    let contentNode = $slots.content;
     if (!contentNode) {
-      const contents = []
-      this.showClose && contents.push(<span class="k-modal-close" onClick={this.close}><Icon type={Close} strokeWidth={50} /></span>)
-      this.title !== null && contents.push(<div class="k-modal-header" ref="hRef"><div class="k-modal-header-inner">{this.title}</div></div>)
-      contents.push(<div class="k-modal-body">{$slots.default}</div>)
+      const contents = [];
+      this.showClose &&
+        contents.push(
+          <span class="k-modal-close" onClick={this.close}>
+            <Icon type={Close} strokeWidth={50} />
+          </span>
+        );
+      this.title !== null &&
+        contents.push(
+          <div class="k-modal-header" ref="hRef">
+            <div class="k-modal-header-inner">{this.title}</div>
+          </div>
+        );
+      contents.push(<div class="k-modal-body">{$slots.default}</div>);
 
       //footer
       if (this.footer !== null) {
-        let footer = $slots.footer
+        let footer = $slots.footer;
         if (!footer) {
-          footer = [<Button onClick={this.cancel}>{cancelText}</Button>, <Button onClick={this.ok} type="primary" loading={this.loading}>{okText}</Button>]
+          footer = [
+            <Button onClick={this.cancel}>{cancelText}</Button>,
+            <Button onClick={this.ok} type="primary" loading={this.loading}>
+              {okText}
+            </Button>,
+          ];
         }
         const footerNode = footer ? <div class="k-modal-footer">{footer}</div> : null;
 
-        contents.push(footerNode)
+        contents.push(footerNode);
       }
-      contentNode = <div class="k-modal-content">{contents}</div>
+      contentNode = <div class="k-modal-content">{contents}</div>;
     }
 
     const style = {
       width: `${this.width}px`,
       top: `${this.currentTop}px`,
       left: `${this.left}px`,
-    }
+    };
     const classes = [
-      'k-modal', {
-        'k-modal-draggable': draggable,
-        'k-modal-maximized': this.maximized,
-        'k-modal-centered': this.centered,
-        'k-modal-has-footer': this.footer !== null,
-      }
-    ]
-    return this.rendered ? (<div class={classes} v-transfer={transfer}>
-      {maskNode}
-      <div class="k-modal-wrap" v-show={showInner} onClick={this.clickMaskToClose}>
-        <transition name="k-modal-zoom">
-          <div class="k-modal-inner" ref="modal" v-show={show} style={style}>
-            {contentNode}
-          </div>
-        </transition>
+      "k-modal",
+      {
+        "k-modal-draggable": draggable,
+        "k-modal-maximized": this.maximized,
+        "k-modal-centered": this.centered,
+        "k-modal-has-footer": this.footer !== null,
+      },
+    ];
+    return this.rendered ? (
+      <div class={classes} v-transfer={transfer}>
+        {maskNode}
+        <div class="k-modal-wrap" v-show={showInner} onClick={this.clickMaskToClose}>
+          <transition name="k-modal-zoom">
+            <div class="k-modal-inner" ref="modal" v-show={show} style={style}>
+              {contentNode}
+            </div>
+          </transition>
+        </div>
       </div>
-    </div>) : null
-  }
-}
+    ) : null;
+  },
+};
