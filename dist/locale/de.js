3d583f7e5ce440bc1ac938a2ff4f436a5f3b9001
Qiu <smallerqiu@gmail.com>
2025-06-09 19:02:09 +0800

    test build

--- dist/locale/de.js
+++ dist/locale/de.js
@@ -1,9 +1,7 @@
 /*!
- * kui-vue v3.4.2
- *  Copyright 2017-present, kui-vue.
- *  All rights reserved.
- *  Docs: https://k-ui.cn
- * Author: chuchur@qq.com / www.chuchur.com
- *
+ * kui-vue v3.4.9
+ * Copyright 2017-present, kui-vue.
+ * All rights reserved.
+ * Author: Qiu / www.chuchur.com
  */
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define("kui_lang_de",[],t):"object"==typeof exports?exports.kui_lang_de=t():e.kui_lang_de=t()}(this,(function(){return e={314:function(e){e.exports={k:{datePicker:{startDate:"Startdatum",endDate:"Enddatum",placeholder:"bitte wählen",months:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],weeks:["Su","Mo","Tu","We","Th","Fr","Sa"],year:"",month:"",day:"",today:"Heute",now:"aktuelle Zeit",ok:"Okay",selectDate:"bitte wählen",selectTime:"bitte wählen",back:"zurück"},drawer:{ok:"Okay",cancel:"Abbrechen"},empty:{description:"noch keine Daten"},pop:{ok:"Okay",cancel:"Abbrechen"},modal:{ok:"Okay",cancel:"Abbrechen"},page:{page:"Seite",pageSize:"item/page",goto:"Spring zu",items:"items",total:"Total"},select:{placeholder:"bitte wählen",loading:"Laden...",emptyText:"noch keine Daten"},table:{emptyText:"noch keine Daten"},upload:{successful:"Erfolg",failed:"gescheitert",errorFileSize:"illegale Dateigröße"},image:{preview:"Vorschau"}}}}},t={},function a(o){var n=t[o];if(void 0!==n)return n.exports;var r=t[o]={exports:{}};return e[o](r,r.exports,a),r.exports}(314);var e,t}));
+(function(e,t){"object"===typeof exports&&"object"===typeof module?module.exports=t():"function"===typeof define&&define.amd?define("kui_lang_de",[],t):"object"===typeof exports?exports["kui_lang_de"]=t():e["kui_lang_de"]=t()})("undefined"!==typeof self?self:this,(function(){return function(){var e={258:function(e){e.exports={k:{datePicker:{startDate:"Startdatum",endDate:"Enddatum",placeholder:"bitte wählen",months:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],weeks:["Su","Mo","Tu","We","Th","Fr","Sa"],year:"",month:"",day:"",today:"Heute",now:"aktuelle Zeit",ok:"Okay",selectDate:"bitte wählen",selectTime:"bitte wählen",back:"zurück"},drawer:{ok:"Okay",cancel:"Abbrechen"},empty:{description:"noch keine Daten"},pop:{ok:"Okay",cancel:"Abbrechen"},modal:{ok:"Okay",cancel:"Abbrechen"},page:{page:"Seite",pageSize:"item/page",goto:"Spring zu",items:"items",total:"Total"},select:{placeholder:"bitte wählen",loading:"Laden...",emptyText:"noch keine Daten"},table:{emptyText:"noch keine Daten"},upload:{successful:"Erfolg",failed:"gescheitert",errorFileSize:"illegale Dateigröße"},image:{preview:"Vorschau"}}}}},t={};function a(n){var o=t[n];if(void 0!==o)return o.exports;var r=t[n]={exports:{}};return e[n](r,r.exports,a),r.exports}var n=a(258);return n}()}));
