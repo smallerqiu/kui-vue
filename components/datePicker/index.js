a951aaafce5500a0b32035ea633f9dc2ca3942a7
smallerqiu <smallerqiu@gmail.com>
2025-06-26 23:05:24 +0800

    nothing

--- components/datePicker/index.js
+++ components/datePicker/index.js
@@ -1,4 +1,3 @@
-/* istanbul ignore file */
 import DatePicker from './DatePicker';
 import Calendar from './calendar/calendar';
 import CalendarRange from './calendar/range';
@@ -6,10 +5,8 @@ import TimePanel from './time/panel';
 import TimeRange from './time/range';
 import DateTime from './datetime/datetime';
 import DateTimeRange from './datetime/range';
-import { locale } from './locale';
 
 const api = {
-  locale,
   install(app) {
     app.component('DatePicker', DatePicker);
   },
