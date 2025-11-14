6620ad41234450d7149be415ffdcfeee5c768365
chuchur <chuchur@qq.com>
2025-11-14 18:59:57 +0800

    fix:spell

--- components/image/group.jsx
+++ components/image/group.jsx
@@ -1,29 +1,22 @@
-
-import { withInstall } from '../utils/vue'
+import { withInstall } from "../utils/vue";
+import { ref, provide } from "vue";
 const ImageGroup = {
-  name: 'ImageGroup',
-  provide() {
-    return {
-      ImageGroup: this,
-    }
-  },
-  data() {
-    return {
-      data: []
-    }
-  },
-  methods: {
-    updateCollection(mount, src) {
+  name: "ImageGroup",
+  setup(props, { slots }) {
+    const data = ref([]);
+    const updateCollection = (mount, src) => {
       if (mount) {
-        this.data.push(src)
+        this.data.push(src);
       } else {
-        let index = this.data.indexOf(src)
-        this.data.splice(index, 1)
+        let index = this.data.indexOf(src);
+        this.data.splice(index, 1);
       }
-    }
+    };
+    provide("ImageGroup", { data, updateCollection });
+    
+    return () => {
+      return <div class="k-image-group">{slots.default?.()}</div>;
+    };
   },
-  render() {
-    return <div class="k-image-group">{this.$slots.default}</div>
-  }
-}
-export default withInstall(ImageGroup)
+};
+export default withInstall(ImageGroup);
