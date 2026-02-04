28496f8b0bf7d69eb570ef9bc3d1c3edb3e9dd8a
chuchur <chuchur@qq.com>
2026-02-04 22:22:26 +0800

    docs: add tip

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,17 @@
 import { ref } from "vue";
 import en from "kui-vue/locale/en";
 const locale = ref(en);
+import { message } from "kui-vue";
+const iknow = localStorage.getItem("iknow-v4");
+if (!iknow) {
+  message.show({
+    type: "warning",
+    duration: 0,
+    closable: true,
+    content: "当前文档为v4.x 版本，不支持Vue2, Vue2 项目请使用v3.x ！",
+    onClose: () => {
+      localStorage.setItem("iknow", "yes");
+    },
+  });
+}
 </script>
